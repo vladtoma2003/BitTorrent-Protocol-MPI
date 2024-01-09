31efad0ba1a717ca49d104dbeb770c0f0af4a94a
Vlad Iliescu <vladtoma.iliescu@gmail.com>
2024-01-09 12:00:40 +0200

    Started
    - Added structures
    - Added readInput function
    - Added comments for functionality

--- src/tema3.c
+++ src/tema3.c
@@ -9,9 +9,35 @@
 #define HASH_SIZE 32
 #define MAX_CHUNKS 100
 
+typedef struct {
+    char filename[MAX_FILENAME];
+    int chunks_count;
+    char chunks[MAX_CHUNKS][HASH_SIZE];
+} file;
+
+typedef struct {
+    int files_count;
+    file files[MAX_FILES];
+} files_list;
+
+readInput(char *filename);
+
+/*
+    Steps:
+    1. Get the list of seeds/peers for files from tracker
+    2. Check missing chunks and search for them in the list of seeds
+    3. Foreach missing chunk, follow the steps:
+        3.1. Pick a seed/peer
+        3.2. Request the chunk from the seed/peer
+        3.3. Wait for the chunk
+        3.4. Mark as received
+
+*/
 void *download_thread_func(void *arg)
 {
     int rank = *(int*) arg;
+
+    
 
     return NULL;
 }
@@ -25,8 +51,14 @@ void *upload_thread_func(void *arg)
 
 void tracker(int numtasks, int rank) {
 
+
+
 }
 
+/*
+    It has the segments of the file. It handles the download and upload of the file.
+    It's main priority: Shares the owned segments with other peers and gets the missing ones.
+*/
 void peer(int numtasks, int rank) {
     pthread_t download_thread;
     pthread_t upload_thread;
@@ -77,4 +109,39 @@ int main (int argc, char *argv[]) {
     }
 
     MPI_Finalize();
+}
+
+/*
+    Reads the input. Structure of the file:
+    - first line: No. of files
+    - next line: filname hash chunks_count
+    - next lines: chunk_1, chunk_2, ..., chunk_n
+    - next line: filname hash chunks_count
+    - next lines: chunk_1, chunk_2, ..., chunk_n
+    ...
+*/
+files_list *readInput(char *filename) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        printf("Eroare la deschiderea fisierului %s\n", filename);
+        exit(-1);
+    }
+
+    files_list *files = (files_list *) malloc(sizeof(files_list));
+    if (files == NULL) {
+        printf("Eroare la alocarea memoriei pentru files\n");
+        exit(-1);
+    }
+
+    fscanf(f, "%d", &files->files_count);
+    for (int i = 0; i < files->files_count; i++) {
+        fscanf(f, "%s", files->files[i].filename);
+        fscanf(f, "%d", &files->files[i].chunks_count);
+        for (int j = 0; j < files->files[i].chunks_count; j++) {
+            fscanf(f, "%s", files->files[i].chunks[j]);
+        }
+    }
+
+    fclose(f);
+    return files;
 }
