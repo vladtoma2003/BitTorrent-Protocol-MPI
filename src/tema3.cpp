8091d9c171be56d39494686c91aaa6b31832c960
Vlad Iliescu <vladtoma.iliescu@gmail.com>
2024-01-11 22:07:43 +0200

    Sa mor de stiu ce nu merge

--- src/tema3.cpp
+++ src/tema3.cpp
@@ -10,17 +10,114 @@
         3.3. Wait for the chunk
         3.4. Mark as received
 
+    Tag for download: 100
 */
 void *download_thread_func(void *arg)
 {
-    int rank = *(int*) arg;
+    client *client_data = (client*) arg;
+
+    if(client_data->wantedFilesNo == 0) {
+        return NULL;
+    }
+
+    // foreach wanted file
+    for(int i = 0; i < client_data->wantedFilesNo; ++i) {
+        std::string wantedFile = client_data->wantedFiles[i];
+        int signal = 1;
+        MPI_Send(&signal, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        MPI_Send(&client_data->rank, 1, MPI_INT, TRACKER_RANK, 100, MPI_COMM_WORLD);
+        MPI_Send(&wantedFile, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 100, MPI_COMM_WORLD);
+
+        int seeds_no;
+        MPI_Recv(&seeds_no, 1, MPI_INT, TRACKER_RANK, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::vector<int> seeds;
+        for(int j = 0; j < seeds_no; ++j) {
+            int seed_rank;
+            MPI_Recv(&seed_rank, 1, MPI_INT, TRACKER_RANK, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            seeds.push_back(seed_rank);
+        }
+
+        // I got the list of peers that have my file
+        int counter = 0;
+
+        int first_person = seeds[0];
+
+        int upload_signal = 100;
+        MPI_Send(&signal, 1, MPI_INT, first_person, 200, MPI_COMM_WORLD);
+        request req;
+        req.source = client_data->rank;
+        strcpy(req.filename, wantedFile.c_str());
+        req.chunk_no = -1;
+        MPI_Send(&req, sizeof(request), MPI_CHAR, first_person, 200, MPI_COMM_WORLD);
+
+        int chunk_no;
+        MPI_Recv(&chunk_no, 1, MPI_INT, first_person, 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        std::vector<std::string> chunks;
+        int peers_no = seeds.size();
+        int divided_chunks = chunk_no / peers_no;
+
+        // divide the chunks between the peers. The first divided_chunks number of chunks will come from the first peer and so on
+        std::vector<std::string> chunks_received;
+        for(int j = 0; j < chunk_no; ++j) {
+            MPI_Send(&upload_signal, 1, MPI_INT, seeds[j / divided_chunks], 200, MPI_COMM_WORLD);
+            request new_req;
+            new_req.source = client_data->rank;
+            strcpy(new_req.filename, wantedFile.c_str());
+            new_req.chunk_no = j;
+            MPI_Send(&new_req, sizeof(request), MPI_CHAR, seeds[j / divided_chunks], 200, MPI_COMM_WORLD);
+
+            std::string chunk;
+            MPI_Recv(&chunk, HASH_SIZE, MPI_CHAR, seeds[j / divided_chunks], 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            chunks_received.push_back(chunk);
+        }
+
+        client_data->files[wantedFile].chunks = chunks_received;
+        client_data->files[wantedFile].no_chunks = chunks_received.size();
+        client_data->files[wantedFile].filename = wantedFile;
+        
+        // send to tracker that I finished downloading the file
+        signal = 2;
+        MPI_Send(&signal, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        MPI_Send(&client_data->rank, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        MPI_Send(&wantedFile, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+
+    }
+
+    int signal = 3;
+    MPI_Send(&signal, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
 
     return NULL;
 }
 
+/*
+    Tag for upload: 200
+*/
 void *upload_thread_func(void *arg)
 {
-    int rank = *(int*) arg;
+    // int rank = *(int*) arg;
+    client *client_data = (client*) arg;
+
+    // signals: 100 - request, 200 - done
+    while(1) {
+        int signal;
+        MPI_Recv(&signal, 1, MPI_INT, MPI_ANY_SOURCE, 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if(signal == 100) {
+            request req;
+            MPI_Recv(&req, sizeof(request), MPI_CHAR, MPI_ANY_SOURCE, 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(req.chunk_no == -1) {
+                // send the number of chunks
+                int chunks_no = client_data->files[req.filename].no_chunks;
+                MPI_Send(&chunks_no, 1, MPI_INT, req.source, 200, MPI_COMM_WORLD);
+            } else {
+                std::string chunk = client_data->files[req.filename].chunks[req.chunk_no];
+                MPI_Send(&chunk, HASH_SIZE, MPI_CHAR, req.source, 200, MPI_COMM_WORLD);
+            }
+        } else if(signal == 200) { // all peers finished downloading
+            break;
+        }
+    }
+
 
     return NULL;
 }
@@ -40,9 +137,79 @@ void tracker(int numtasks, int rank) {
     // Recieves from each peer a list of the files it has
     std::map<int, std::vector<std::string>> peerFiles; // rank -> files
     
+    for(int i = 1; i < numtasks; ++i) { // Gets the list of owned files from each peer
+        int files_no;
+        int current_rank;
+        MPI_Recv(&current_rank, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&files_no, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        for(int j = 0; j < files_no; ++j) {
+            char filename[100];
+            MPI_Recv(&filename, 100, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            peerFiles[current_rank].push_back(filename);
+        }   
+    }
+
+    for(auto it = peerFiles.begin(); it != peerFiles.end(); ++it) {
+        std::cout << it->first << ":\n";
+        for(auto it2 = it->second.begin(); it2 != it->second.end(); ++it2) {
+            std::cout << *it2 << "\n";
+        }
+    }
+
+
+    int finished = 0;
+
+    while(1) {
+        int signal;
+        MPI_Recv(&signal, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // signals: 1 - request, 2 - finished download, 3 - finished all downloads
+        if(signal == 1) {
+            int source;
+            MPI_Recv(&source, 1, MPI_INT, MPI_ANY_SOURCE, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::string filename;
+            MPI_Recv(&filename, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            std::vector<int> seeds; // ranks of seeds that have the file
+            for(auto it = peerFiles.begin(); it != peerFiles.end(); ++it) {
+                for(auto it2 = it->second.begin(); it2 != it->second.end(); ++it2) {
+                    if(*it2 == filename) {
+                        seeds.push_back(it->first);
+                    }
+                }
+            }
+
+            int seeds_no = seeds.size();
+
+            MPI_Send(&seeds_no, 1, MPI_INT, source, 100, MPI_COMM_WORLD);
+            for(int i = 0; i < seeds_no; ++i) {
+                int seed_rank = seeds[i];
+                MPI_Send(&seed_rank, 1, MPI_INT, source, 100, MPI_COMM_WORLD);
+            }
+        } else if(signal == 2) {
+            // a peer finished downloading a file
+            int source;
+            MPI_Recv(&source, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            std::string filename;
+            MPI_Recv(&filename, MAX_FILENAME, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // mark the peer as seed
+            peerFiles[source].push_back(filename);
+
+        } else if (signal == 3) { // a peer finished downloading all the files
+            finished++;
+            if(finished == numtasks - 1) {
+                // send stop signal to uploaders
+                int signal = 200;
+                for(int i = 1; i < numtasks; ++i) {
+                    MPI_Send(&signal, 1, MPI_INT, i, 200, MPI_COMM_WORLD);
+                }
+                break;
+            }
+
+        }
+    }
     
-
-
 }
 
 /*
@@ -57,14 +224,15 @@ void peer(int numtasks, int rank) {
 
     client cv = readInput(rank);
     // cv.printFiles();
+    cv.sendDataToTracker();
 
-    r = pthread_create(&download_thread, NULL, download_thread_func, (void *) &rank);
+    r = pthread_create(&download_thread, NULL, download_thread_func, (void*) &cv);
     if (r) {
         printf("Eroare la crearea thread-ului de download\n");
         exit(-1);
     }
 
-    r = pthread_create(&upload_thread, NULL, upload_thread_func, (void *) &rank);
+    r = pthread_create(&upload_thread, NULL, upload_thread_func, (void *) &cv);
     if (r) {
         printf("Eroare la crearea thread-ului de upload\n");
         exit(-1);
--- src/tema3.h
+++ src/tema3.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include<algorithm>
 
 #define TRACKER_RANK 0
 #define MAX_FILES 10
@@ -13,17 +14,24 @@
 #define HASH_SIZE 32
 #define MAX_CHUNKS 100
 
+struct request {
+    int source;
+    char filename[MAX_FILENAME];
+    int chunk_no;
+};
+
 struct file{
     std::string filename;
     int no_chunks;
     std::vector<std::string> chunks;
 };
 
+
 struct client{
 
     int rank;
     int files_no;
-    std::map<std::string, std::vector<file>> files; // filename -> chunks
+    std::map<std::string, file> files; // filename -> chunks
 
     int wantedFilesNo;
     std::vector<std::string> wantedFiles;
@@ -32,33 +40,34 @@ struct client{
     Prints the files list
     */
     void printFiles() {
-        std::cout << "=================" << rank << "=================\n";
-        std::cout << "Files no: " << files_no << "\n";
+        std::cout << "===================" << rank << "====================" << std::endl;
+        std::cout << "Files: " << std::endl;
         for(auto it = files.begin(); it != files.end(); ++it) {
-            std::cout << it->first << " " << it->second.size() << "\n";
-            for(int i = 0; i < it->second.size(); ++i) {
-                std::cout << it->second[i].filename << " " << it->second[i].no_chunks << "\n";
-                for(int j = 0; j < it->second[i].no_chunks; ++j) {
-                    std::cout << it->second[i].chunks[j] << "\n";
-                }
+            std::cout << it->first << " " << it->second.no_chunks << std::endl;
+            for(int i = 0; i < it->second.no_chunks; ++i) {
+                std::cout << it->second.chunks[i] << " ";
             }
+            std::cout << std::endl;
         }
 
-        std::cout << "Wanted files no: " << wantedFilesNo << "\n";
+        std::cout << "Wanted files: " << std::endl;
         for(int i = 0; i < wantedFilesNo; ++i) {
-            std::cout << wantedFiles[i] << "\n";
+            std::cout << wantedFiles[i] << std::endl;
         }
     }
 
     /*
         Sends the list of owned files to the tracker
     */
-    // void sendDataToTracker() {
-    //     for(int i = 0; i < files_no; ++i) {
-    //         MPI_Send(&(files[i].filename.c_str()), (files[i].filename.size), MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
-    //     }
-    // }
+    void sendDataToTracker() {
+        MPI_Send(&rank, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        MPI_Send(&files_no, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        for(auto it = files.begin(); it != files.end(); ++it) {
+            MPI_Send(it->first.c_str(), MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        }
+    }
 };
+
 
 /*
 Reads the input. Structure of the file:
@@ -68,6 +77,8 @@ Reads the input. Structure of the file:
 - next line: filname hash chunks_count
 - next lines: chunk_1, chunk_2, ..., chunk_n
 ...
+- next line: No. of wanted files
+- next lines: wanted_file_1, wanted_file_2, ..., wanted_file_n
 */
 client readInput(int rank) {
     client c;
@@ -86,7 +97,7 @@ client readInput(int rank) {
             std::getline(f, line);
             cur.chunks.push_back(line);
         }
-        c.files[cur.filename].push_back(cur);
+        c.files[cur.filename] = cur;
     }
 
     std::getline(f, line);
